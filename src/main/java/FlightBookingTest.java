0617d554cb259c2312ddb9e5b1e9fe86f4675f66
Nikita-Bhopardikar <bhopardikar.nikita@gmail.com>
2018-04-22 13:23:04 +0530

    Debug: Changed the locator(Id) of destination field, Changed the locator(xpath) of Date picker field

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -22,27 +21,31 @@ public class FlightBookingTest {
         driver.get("https://www.cleartrip.com/");
         waitFor(2000);
         driver.findElement(By.id("OneWay")).click();
-
+        
         driver.findElement(By.id("FromTag")).clear();
         driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
 
         //wait for the auto complete options to appear for the origin
-
         waitFor(2000);
+        
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        // changed Destination field locator:Id from "toTag" to "ToTag"
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
-
         waitFor(2000);
+        
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        //getting the list of valid dates and selecting the first one which would be "Today's date" 
+        List<WebElement> dateSelected = driver.findElements(By.xpath("//*[@id='ui-datepicker-div']//td[not(contains(class,'-unselectable disabled'))]/a"));
+        dateSelected.get(0).click();
+        
 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
